d4133ecd348e3d9d0f17af26f4a195f3f8a1da20
bno1 <ealex95@gmail.com>
2016-12-02 02:26:55 +0200

    coding style changes

--- d01/main.hs
+++ d01/main.hs
@@ -4,78 +4,79 @@ import Data.Maybe (isNothing)
 import Data.Char (isDigit)
 import Data.Set (Set, empty, member, insert)
 
-type Step = (Char, Int)
+type Instruction = (Char, Int)
+type Coord = (Int, Int)
 
-data Position = Position {
-	pos :: (Int, Int),
-	dir :: (Int, Int),
-	visited :: Set (Int, Int),  -- set of visited positions
-	vtwice :: Maybe (Int, Int)  -- first position visited twice
+data PState = PState {
+    pstatePos :: Coord,
+    pstateDir :: Coord,
+    pstateVisited :: Set Coord,  -- set of visited positions
+    pstateVTwice :: Maybe Coord  -- first position visited twice
 }
 
-initialState :: Position
-initialState = Position (0, 0) (1, 0) empty Nothing
+instance (Num a, Num b) => Num (a, b) where
+    (a1, b1) + (a2, b2) = (a1 + a2, b1 + b2)
+    (a1, b1) * (a2, b2) = (a1 * a2, b1 * b2)
+
+initialState :: PState
+initialState = PState (0, 0) (1, 0) empty Nothing
 
 -- parses "L2, R5, ..." into [('L', 2), ('R', 5), ...]
-parseInput :: String -> [Step]
-parseInput =
-	map (head &&& read . takeWhile isDigit . tail) . words
+parseInput :: String -> [Instruction]
+parseInput = map (head &&& read . takeWhile isDigit . tail) . words
 
 -- turns the first argument (vector) by 90 degrees
-turn :: (Int, Int) -> Char -> (Int, Int)
+turn :: Coord -> Char -> Coord
 turn (dr, dc) 'L' = (dc, -dr)
 turn (dr, dc) 'R' = (-dc, dr)
 turn d _ = d
 
 -- manhattan distance of a point from initial position
-distance :: (Int, Int) -> Int
-distance (r, c) = abs (r - ri) + abs (c - ci)
-	where (ri, ci) = pos initialState
+distance :: Coord -> Int
+distance pos = abs r + abs c
+    where (r, c) = pos + pstatePos initialState
+
+-- returns the list of positions visited by following the instruction
+visitedPositions :: Coord -> Coord -> Int -> [Coord]
+visitedPositions pos dir dist = tail . scanl (+) pos . replicate dist $ dir
 
 -- runs an instruction
-step :: Step -> State Position (Int, Int)
-step (t, d) = let
-		-- for every position it checks if it's already in the set
-		-- if it is then it stops and returns it
-		-- else it inserts it and continues
-		updateSet :: [(Int, Int)] -> State (Set (Int, Int)) (Maybe (Int, Int))
-		updateSet [] = return Nothing
-		updateSet (p:ps) = do
-			s <- get
-			if member p s then return $ Just p
-			else put (insert p s) >> updateSet ps
+step :: Instruction -> State PState Coord
+step (t, dist) = let
+        -- for every position it checks if it's already in the set
+        -- if it is then it stops and returns it
+        -- else it inserts it and continues
+        updateSet :: [Coord] -> State (Set Coord) (Maybe Coord)
+        updateSet [] = return Nothing
+        updateSet (pos:ps) = do
+            set <- get
+            if member pos set
+                then return $ Just pos
+                else put (insert pos set) >> updateSet ps
+    in do
+        pstate <- get
+        let pos = pstatePos pstate
+        let dir' = turn (pstateDir pstate) t
+        let pos' = pos + dir' * (dist, dist)  -- final pos
 
-		-- returns the list of positions visited by following the instruction
-		-- p = current pos, dp = direction, d = distance
-		computePositions :: (Int, Int) -> (Int, Int) -> Int -> [(Int, Int)]
-		computePositions p dp d = tail $
-			scanl (\(pr, pc) (pdr, pdc) -> (pr + pdr, pc + pdc)) p $ replicate d dp
-	in do
-		position <- get
-		let (r, c) = pos position
-		let (tdr, tdc) = turn (dir position) t
-		let fp = (r + tdr * d, c + tdc * d)  -- final pos
-		let vt = vtwice position
+        modify (\p -> p{pstatePos = pos', pstateDir = dir'})
 
-		modify (\p -> p{pos = fp, dir = (tdr, tdc)})
+        -- if no pstate was visited twice yet
+        when (isNothing $ pstateVTwice pstate) $ do
+            -- compute visited positions by the current instruction
+            let visited = visitedPositions pos dir' dist
 
-		-- if no position was visited twice yet
-		when (isNothing vt) $ do
-			let vps = visited position
+            -- update the visited set and the visited twice position
+            let vps = pstateVisited pstate
+            let (vtwice', visited') = runState (updateSet visited) vps
 
-			-- compute visited positions by the current instruction
-			let positions = computePositions (r, c) (tdr, tdc) d
+            modify (\p -> p{pstateVisited = visited', pstateVTwice = vtwice'})
 
-			-- update the visited set and the visited twice position
-			let (newvtwice, newvisited) = runState (updateSet positions) vps
-
-			modify (\p -> p{visited = newvisited, vtwice = newvtwice})
-
-		return fp
+        return pos'
 
 main :: IO ()
 main = do
-	contents <- readFile "input.txt"
-	let steps = parseInput contents
-	let p = foldl (\a b -> execState (step b) a) initialState steps
-	print (distance $ pos p, distance <$> vtwice p)
+    contents <- readFile "input.txt"
+    let steps = parseInput contents
+    let pstate = foldl (\a b -> execState (step b) a) initialState steps
+    print (distance $ pstatePos pstate, distance <$> pstateVTwice pstate)
