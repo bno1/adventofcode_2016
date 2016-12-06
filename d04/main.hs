a3ecbd194d954a60afbe52f561c79d559f9bde14
bno1 <ealex95@gmail.com>
2016-12-06 17:19:31 +0200

    comments for d04 and d05

--- d04/main.hs
+++ d04/main.hs
@@ -2,6 +2,7 @@ import Data.List
 import Data.Char (isLower, chr, ord)
 import Control.Arrow ((&&&))
 
+-- door description
 data Door = Door {
     doorName :: [String],
     doorId :: Int,
@@ -11,6 +12,8 @@ data Door = Door {
 lowercaseLetters :: String
 lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
 
+-- split list at every occurence of an element
+-- split "a bcd efgh    ijkl" ' ' => ["a", "bcd", "efgh", "ijkl"]
 split :: Eq a => [a] -> a -> [[a]]
 split [] _ = []
 split lst b
@@ -18,6 +21,7 @@ split lst b
     | otherwise = x : split xs b
         where (x, xs) = span (/= b) lst
 
+-- perses a line describing a door
 parseDoor :: String -> Door
 parseDoor str = let
         parts = split str '-'
@@ -27,23 +31,32 @@ parseDoor str = let
     in
         Door dName dId dChecksum
 
+-- computes a door's checksum
 computeChecksum :: Door -> String
 computeChecksum door = let
+        -- counts the occurences of chr c in the door name
         countOccurences c = sum . map (length . filter (== c)) $ doorName door
+
+        -- [(chr, occurences of chr in door name)]
         occurences = map (id &&& countOccurences) lowercaseLetters
+
+        -- order by occurence count decresing then by chr increasing
         orderFunc (c1, o1) (c2, o2)
             | o1 == o2 = compare c1 c2
             | otherwise = compare o2 o1
     in
         map fst . take 5 . sortBy orderFunc $ occurences
 
+-- check if the checksum is correct
 checkDoor :: Door -> Bool
 checkDoor door = computeChecksum door == doorChecksum door
 
+-- shift the character for the shift cipher
 shiftChar :: Int -> Char -> Char
 shiftChar shift ch =
     chr $ ((ord ch - ord 'a' + shift) `rem` length lowercaseLetters) + ord 'a'
 
+-- decrypt door name
 decryptDoor :: Door -> Door
 decryptDoor door = let
         dcName = map (map (shiftChar (doorId door))) $ doorName door
@@ -54,12 +67,18 @@ main :: IO ()
 main = do
     contents <- readFile "input.txt"
     let doors = map parseDoor $ lines contents
+
+    -- filter invalid doors
     let realDoors = filter checkDoor doors
 
     let answer1 = sum . map doorId $ realDoors
     print answer1
 
+    -- decrypt doors
     let decryptedDoors = map decryptDoor realDoors
+
+    -- find the door containing "northpole" in name
     let northpoleStorage = find (elem "northpole" . doorName) decryptedDoors
 
+    -- print northpoleStorage door name and id
     print $ fmap ((unwords . doorName) &&& doorId) northpoleStorage
--- d05/main.hs
+++ d05/main.hs
@@ -4,9 +4,13 @@ import Data.ByteString.Char8 (pack)
 import Data.Word (Word64)
 import Data.Char (digitToInt)
 
+-- compute md5 of a bytestring
 md5 :: ByteString -> Digest MD5
 md5 = hash
 
+-- takes a string and a starting nounce
+-- finds the first nounce such that md5(strign + nounce) starts with 5 zeros
+-- returns the md5 digest and the found nounce + 1
 nextChar :: String -> Word64 -> (String, Word64)
 nextChar str nounce = do
     let digest = show . md5 $ pack (str ++ show nounce)
@@ -15,6 +19,9 @@ nextChar str nounce = do
         then (digest, nounce + 1)
         else nextChar str (nounce + 1)
 
+-- takes a string and a md5 digest, returns a modified string
+-- p = digest[5], c = digest[6]
+-- if string[p] =/= '_' then string[6] = c
 applyDigest :: String -> String -> String
 applyDigest str digest = let
         p = digitToInt $ digest !! 5
@@ -29,17 +36,26 @@ main :: IO()
 main = do
     door <- fmap (head . lines) $ readFile "input.txt"
 
+    -- list of all digests of type door name + nounce that start with 5 zeros
     let digests = tail $ iterate (nextChar door . snd) ("", 0)
 
     putStrLn "Running phase 1..."
 
+    -- generate characters for first answer, can take a while
     let answer1 = take 8 $ map ((!! 5) . fst) digests
+
+    -- prints characters as they are generated
     mapM_ print answer1
+    -- prints the final complete answer
     putStrLn answer1
 
     putStrLn ""
     putStrLn "Running phase 2..."
 
+    -- generate string for answer 2, can take a while
     let answer2 = scanl applyDigest (replicate 8 '_') $ map fst digests
+
+    -- prints the answer as it is generated (while it still contains '_'s)
     mapM_ putStrLn $ takeWhile (elem '_') answer2
+    -- print the final answer
     putStrLn $ head . filter (notElem '_') $ answer2
